ce85499d203f96b1e6da6e55878a8eb702462228
DeniRomansyah <a2.1900043@mhs.stmik-sumedang.ac.id>
2021-01-07 15:35:10 +0700

    UTS

---
+++ A2.1900043/#08 - UTS/main.py
@@ -0,0 +1,43 @@
+desktop = 5
+# angka_saya telah kita beri nilai 1945
+
+angka_saya = 1945
+
+# setelah didefinisikan dan diberi nilai
+# kita bisa mengubah nilainya
+
+angka_saya = 
+
+# Untuk melihat kalau kita telah mengubahnya,
+# kita akan mencetaknya dengan print
+
+print angka_saya
+Komputer = 1
+    laptop = 2
+IndentationError: expected an
+indented block 
+(<string>, line 2)
+.nama_bahasa = "python"
+
+P3MBU4T = "Guido van Rossum" # nama variabel yang 'alay', tapi apakah valid?
+
+T@hun_dibu@tny@ = 1991
+
+# variabel panjang nih
+contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, dan lain-lain."
+
+$belajar_python_oop = True
+
+3m4ng_s3ru = "Benar"
+class NamaClass(object):
+  # segala sesuatu tentang NamaClass
+class Gadget(object):
+  sumber_energi = "listrik"
+nama_objek = nama_class()
+class Gadget(object):
+    sumber_energi = "listrik"
+
+    hp = Gadget()
+namprint (hp.sumber_energi)
+print (hp.sumber_energi)
+
