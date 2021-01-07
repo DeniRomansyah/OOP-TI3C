bcc8b80e4f7788d501ae77b58d84a978637009ff
DeniRomansyah <a2.1900043@mhs.stmik-sumedang.ac.id>
2021-01-07 15:48:12 +0700

    UTS

--- A2.1900043/#08 - UTS/main.py
+++ A2.1900043/#08 - UTS/main.py
@@ -19,16 +19,16 @@ indented block
 (<string>, line 2)
 .nama_bahasa = "python"
 
-P3MBU4T = "Guido van Rossum" # nama variabel yang 'alay', tapi apakah valid?
+PEMBUAT = "Guido van Rossum" # nama variabel yang 'alay', tapi apakah valid?
 
-T@hun_dibu@tny@ = 1991
+Tahun_dibuatnya = 1991
 
 # variabel panjang nih
 contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, dan lain-lain."
 
 $belajar_python_oop = True
 
-3m4ng_s3ru = "Benar"
+Emang_seru = "Benar"
 class NamaClass(object):
   # segala sesuatu tentang NamaClass
 class Gadget(object):
